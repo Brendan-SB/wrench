acf38b8d9f8ad4fe0442b26f7767e271a30d9497
Brendan <brendansbortz@gmail.com>
2021-12-10 17:28:51 -0500

    better

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -16,7 +16,7 @@ struct LightArray {
 layout(location = 0) in vec3 v_normal;
 layout(location = 1) in vec2 tex_coord;
 layout(location = 2) in vec3 f_pos;
-layout(location = 3) in mat4 cam_view;
+layout(location = 3) in mat4 cam_translation;
 
 layout(location = 0) out vec4 f_color;
 
@@ -32,7 +32,7 @@ void main() {
     vec4 brightness = vec4(uniforms.ambient);
 
     for (uint i = 0; i < uniforms.lights.len; i++) {
-        brightness += dot(v_normal, normalize(uniforms.lights.array[i].position - f_pos)) * uniforms.lights.array[i].intensity * uniforms.lights.array[i].color;
+        brightness += dot(v_normal, normalize(mat3(-cam_translation) * uniforms.lights.array[i].position - f_pos)) * uniforms.lights.array[i].intensity * uniforms.lights.array[i].color;
     }
     
     f_color = texture(tex, tex_coord) * uniforms.color * brightness;
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -7,7 +7,7 @@ layout(location = 2) in vec3 normal;
 layout(location = 0) out vec3 v_normal;
 layout(location = 1) out vec2 tex_coords;
 layout(location = 2) out vec3 f_pos;
-layout(location = 3) out mat4 cv;
+layout(location = 3) out mat4 cam_translation;
 
 layout(set = 0, binding = 0) uniform Data {
     mat4 rotation;
@@ -27,6 +27,6 @@ void main() {
     v_normal = normalize(normal);
     tex_coords = uv;
     f_pos = vec3(global_transform * vec4(position, 1.0));
-    cv = uniforms.cam_translation * uniforms.translation;
+    cam_translation = uniforms.cam_translation;
     gl_Position = uniforms.proj * world_view * uniforms.scale * vec4(position, 1.0);
 }

ccce541ed06a13af28b6a95bb6778a4655396a1f
Brendan <brendansbortz@gmail.com>
2022-02-22 14:11:46 -0500

    add

---
+++ src/shaders/shaders.rs
@@ -0,0 +1,18 @@
+use super::{depth, fragment, vertex};
+use crate::error::Error;
+use std::sync::Arc;
+use vulkano::device::Device;
+
+pub struct Shaders {
+    pub vertex: vertex::Shader,
+    pub fragment: fragment::Shader,
+}
+
+impl Shaders {
+    pub fn new(device: Arc<Device>) -> Result<Arc<Self>, Error> {
+        Ok(Arc::new(Self {
+            vertex: vertex::Shader::load(device.clone())?,
+            fragment: fragment::Shader::load(device.clone())?,
+        }))
+    }
+}

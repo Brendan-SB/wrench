09b844db857d6c2acde8b005049deb1a28c27c37
Brendan <brendansbortz@gmail.com>
2022-03-24 20:10:02 -0400

    better

--- src/assets/mesh.rs
+++ src/assets/mesh.rs
@@ -1,4 +1,5 @@
 use crate::error::Error;
+use cgmath::{InnerSpace, Vector3};
 use obj::TexturedVertex;
 use std::{io::BufRead, sync::Arc};
 use vulkano::{
@@ -17,6 +18,25 @@ vulkano::impl_vertex!(Vertex, position, uv);
 #[derive(Default, Copy, Clone)]
 pub struct Normal {
     pub normal: [f32; 3],
+}
+
+impl Normal {
+    pub fn from_vertices(vertices: &[&Vertex]) -> Option<Self> {
+        match (vertices.get(0), vertices.get(1), vertices.get(2)) {
+            (Some(v1), Some(v2), Some(v3)) => {
+                let v1 = Vector3::from(v1.position);
+                let v2 = Vector3::from(v2.position);
+                let v3 = Vector3::from(v3.position);
+                let a = v1 - v2;
+                let b = v1 - v3;
+                let n = a.cross(b).normalize();
+                let normal = Self { normal: n.into() };
+
+                Some(normal)
+            }
+            _ => None,
+        }
+    }
 }
 
 vulkano::impl_vertex!(Normal, normal);
